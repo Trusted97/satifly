53dcbfcf40ebff4ffeab2cf7a51676c74d789de1
Trusted97 <benux9700@gmail.com>
2025-11-27 17:01:16 +0100

    chore: simplify logic for ProcessFactory

---
+++ src/Process/EnvironmentProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Process;
+
+final readonly class EnvironmentProvider
+{
+    public function __construct(
+        private string $composerHome,
+    ) {
+    }
+
+    public function getEnv(): array
+    {
+        $env = [];
+
+        foreach ($_SERVER as $key => $value) {
+            if (\is_string($value) && false !== $envValue = \getenv($key)) {
+                $env[$key] = $envValue;
+            }
+        }
+
+        foreach ($_ENV as $key => $value) {
+            if (\is_string($value)) {
+                $env[$key] = $value;
+            }
+        }
+
+        $env['COMPOSER_HOME'] ??= $this->composerHome;
+        $env['COMPOSER_NO_INTERACTION'] = '1';
+
+        return $env;
+    }
+}
